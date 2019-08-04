a4eaec7a7653fcbf489cb6f229da07bcc4a4664d
Julian Chu <yulang.chu@gmail.com>
2019-08-05 01:07:57 +0800

    pass :period_no_overlap_before_budget_firstday

--- budgetservice/budget.go
+++ budgetservice/budget.go
@@ -18,3 +18,11 @@ func (b Budget) GetDays() float64 {
 	}
 	return float64(date.AddDate(0, 1, 0).Sub(date).Hours() / 24)
 }
+
+func (b Budget) FirstDay() time.Time {
+	date, err := time.Parse("200601", b.YearMonth)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	return date
+}
