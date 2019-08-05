6851cc5792e89347639526f04eef4047005af3af
Julian Chu <yulang.chu@gmail.com>
2019-08-05 11:37:01 +0800

    refactor

--- budgetservice/budgetservice.go
+++ budgetservice/budgetservice.go
@@ -12,16 +12,18 @@ func NewBudgetService(repo IRepo) *BudgetService {
 
 func (s BudgetService) Query(start time.Time, end time.Time) float64 {
 	budgets := s.repo.GetAll()
-	if len(budgets) == 0 {
-		return 0
-	}
-	if end.Before(budgets[0].FirstDay()) {
-		return 0
-	}
-	if start.After(budgets[0].LastDay()) {
+	if s.isNoBudget(budgets) || s.isNoOverlap(end, budgets, start) {
 		return 0
 	}
 	days := float64(end.Sub(start).Hours()/24 + 1)
 	getDays := budgets[0].GetDays()
 	return (budgets[0].Amount / getDays) * days
+}
+
+func (s BudgetService) isNoBudget(budgets []Budget) bool {
+	return len(budgets) == 0
+}
+
+func (s BudgetService) isNoOverlap(end time.Time, budgets []Budget, start time.Time) bool {
+	return end.Before(budgets[0].FirstDay()) || start.After(budgets[0].LastDay())
 }
