6cf2e9a04ce39d9b004835740537c0d4496fabc0
Julian Chu <yulang.chu@gmail.com>
2019-08-05 16:45:25 +0800

    add test case: invalid_period

--- budgetservice/budgetservice_test.go
+++ budgetservice/budgetservice_test.go
@@ -99,6 +99,21 @@ func Test_period_cross_budget_month(t *testing.T) {
 	assert.Equal(t, expected, actual, "")
 }
 
+func Test_invalid_period(t *testing.T) {
+	m.getAll = func() []Budget {
+		return []Budget{
+			{YearMonth: "201904", Amount: 30},
+		}
+	}
+	actual := service.Query(
+		time.Date(2019, 04, 02, 0, 0, 0, 0, time.UTC),
+		time.Date(2019, 04, 01, 0, 0, 0, 0, time.UTC),
+	)
+
+	expected := float64(0)
+	assert.Equal(t, expected, actual, "")
+}
+
 type MockRepo struct {
 	getAll func() []Budget
 }
