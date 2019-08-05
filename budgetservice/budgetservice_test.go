93f6c8d03738d55c1098f45f14f19cb05dde6360
Julian Chu <yulang.chu@gmail.com>
2019-08-05 11:29:22 +0800

    add no_overlap_after_budget_month

--- budgetservice/budgetservice_test.go
+++ budgetservice/budgetservice_test.go
@@ -54,6 +54,21 @@ func Test_no_overlap_before_budget_firstday(t *testing.T) {
 	assert.Equal(t, expected, actual, "")
 }
 
+func Test_no_overlap_after_budget_lastday(t *testing.T) {
+	m.getAll = func() []Budget {
+		return []Budget{
+			{YearMonth: "201904", Amount: 30},
+		}
+	}
+	actual := service.Query(
+		time.Date(2019, 05, 01, 0, 0, 0, 0, time.UTC),
+		time.Date(2019, 05, 01, 0, 0, 0, 0, time.UTC),
+	)
+
+	expected := float64(0)
+	assert.Equal(t, expected, actual, "")
+}
+
 type MockRepo struct {
 	getAll func() []Budget
 }
